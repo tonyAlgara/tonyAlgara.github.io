b9be7c03fe6d7dcda21c4e70dca057f5c2aa990e
Tony Algara <tony.algara@gmail.com>
2023-09-24 12:19:28 +0200

    Update resume.js

--- resume.js
+++ resume.js
@@ -1,176 +1,142 @@
 const resume = {
-    "name": "John Doe",
-    "title": "Software Engineer and Tech Passionate",
-    "email": "john@doe.com",
-    "phone": "555-555-5555",
-    "address": "123 Main St.",
-    "city": "San Francisco",
-    "state": "CA",
-    "zip": "94101",
-    "profileImg": "https://picsum.photos/200",
-    "website": {
-        "name": "johndoe.com",
-        "url": "http://www.johndoe.com"
-    },
+    "name": "Tony Algara",
+    "title": "React Native Developer & Tech Enthusiast",
+    "email": "--",
+    "phone": "--",
+    "city": "Barcelona",
+    "state": "Catalonia",
+    "profileImg": "https://media.licdn.com/dms/image/C4D03AQHPvstwRtrD_g/profile-displayphoto-shrink_800_800/0/1525167395998?e=1701302400&v=beta&t=ibV9UEsfpDdO1OILZNICTHejFVCF1Z5nBph3_62JEfA",
     "github": {
-        "name": "github.com/johndoe",
-        "url": "http://github.com/johndoe"
+        "name": "github.com/tonyAlgara",
+        "url": "https://github.com/tonyAlgara"
     },
     "linkedin" : {
-        "name": "linkedin.com/johndoe",
-        "url": "http://linkedin.com/in/johndoe"
+        "name": "tony-algara",
+        "url": "www.linkedin.com/in/tony-algara"
     },
-    "summary": "Lorem ipsum <strong>dolor sit amet</strong>, consectetur adipiscing elit. Donec sed odio dui. Nulla vitae elit libero, a pharetra augue. <br><br> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed odio dui. Nulla vitae elit libero, a pharetra augue. <br><br> Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Donec sed odio dui. Nulla vitae elit libero, a pharetra augue.</strong>",
+    "summary": "<strong>Experienced developer with more than 12+ years of experience</strong> in miscellaneous successful projects for web, PWA, and mobile apps, from idea to production. Keen on everyday learning, solving problems, and stepping outside the comfort zone. I'm an empathetic, hard-working, and easy-going person.<br><br><strong>References available upn request.</strong>",
     "education": [
         {
-            "school": "University of California, Berkeley",
-            "degree": "B.A. in Computer Science",
-            "graduation": "2012"
+            "school": "Universitat Oberta de Catalunya",
+            "degree": "Grade, Digital Communication and Media/Multimedia",
+            "graduation": "2019 - In Progress"
         }
     ],
     "work": [
         {
-            "company": "Mario Bros Inc.",
-            "title": "Software Engineer",
-            "years": "2012-2014",
-            "location": "San Francisco, CA",
-            "website": "http://www.mariobros.com",
-            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed odio dui. Nulla vitae elit libero, a pharetra augue.",
+            "company": "Orbit",
+            "title": "React Native Developer",
+            "years": "January 2022 - Present",
+            "location": "Oslo, Norway / Barcelona, Catalonia, Spain (Remote)",
+            "website": "https://www.getorbit.com/",
             "descriptionPoints": [
-                "Created a new feature for the website that increased traffic by 20%.",
-                "Implemented a new system that improved efficiency by 30%.",
-                "Planned and executed a new project that increased revenue by 10%.",
-                "Created a new feature for the website that increased traffic by 20%.",
+                "React Native for mobile application utilizing Javascript and API Rest.",
+                "React for web admin application utilizing TypeScript and GraphQL.",
+                "Developing new features and conducting extensive overhauls of key app components conducting pair programming and code reviews.",
+                "Overseeing the full process from Figma to App Stores.",
+                "Adhering to agile methodologies, including continuous delivery and integration.",
+                "Collaborating closely with the Product team in a fast-paced startup environment."
             ],
-            "keywords": [
-                "Team Lead",
-                "Project Manager",
-                "Scrum Master"
-            ]
+            "keywords": []
         },
         {
-            "company": "Luigi Co.",
-            "title": "Software Engineer",
-            "years": "2012-2014",
-            "location": "San Francisco, CA",
-            "website": "http://www.mariobros.com",
-            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed odio dui. Nulla vitae elit libero, a pharetra augue.",
+            "company": "Emjoy",
+            "title": "React Native / Full Stack Developer",
+            "years": "December 2019 - December 2021",
+            "location": "Barcelona, Catalonia, Spain",
+            "website": "https://www.letsemjoy.com/",
             "descriptionPoints": [
-                "Created a new feature for the website that increased traffic by 20%.",
-                "Worked with a team of 5 to create a new website.",
-                "Planned and executed a new project that increased revenue by 10%."
+                "Developed a mobile application using React Native.",
+                "Created a website using the NextJS framework.",
+                "Used cloud-based Firebase/GCP serverless architectures for backend services.",
+                "Implented agile methodologies, including continuous delivery, pair programming, and code reviews.",
+                "Collaborated closely with varied teams such as Product, Data, and Marketing.",
+                "Operated within a dynamic startup environment."
             ],
-            "keywords": [
-                "Team Lead",
-                "Project Manager",
-                "Scrum Master"
-            ]
+            "keywords": []
         },
         {
-            "company": "Zelda Inc.",
-            "title": "Software Engineer",
-            "years": "2012-2014",
-            "location": "San Francisco, CA",
-            "website": "http://www.mariobros.com",
-            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed odio dui. Nulla vitae elit libero, a pharetra augue.",
+            "company": "CiProtect",
+            "title": "React Developer",
+            "years": "June 2018 - December 2019",
+            "location": "Barcelona, Catalonia, Spain",
             "descriptionPoints": [
-                "Created a new feature for the website that increased traffic by 20%.",
-                "Worked with a team of 5 to create a new website.",
-                "Planned and executed a new project that increased revenue by 10%."
+                "Web application developed using React and Firebase/GCP",
+                "Serving as a product developer for a web application managing and digitally signing documents per GDPR and data protection norms.",
+                "Identifying business opportunities and strategizing functionality and user experience.",
+                "Tailoring the technology stack to optimally meet the application's needs.",
+                "Ensuring the thoughtful design and development of the application."
             ],
-            "keywords": [
-                "Team Lead",
-                "Project Manager",
-                "Scrum Master"
-            ]
+            "keywords": []
         },
         {
-            "company": "Kirby Inc.",
-            "title": "Software Engineer",
-            "years": "2012-2014",
-            "location": "San Francisco, CA",
-            "website": "http://www.mariobros.com",
-            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed odio dui. Nulla vitae elit libero, a pharetra augue.",
+            "company": "OnlyLens.com",
+            "title": "Ecommerce Manager",
+            "years": "August 2015 - Current",
+            "location": "Barcelona, Catalonia, Spain",
+            "website": "https://onlylens.com/",
             "descriptionPoints": [
-                "Created a new feature for the website that increased traffic by 20%.",
-                "Planned and executed a new project that increased revenue by 10%.",
-                "Created a new feature for the website that increased traffic by 20%."
+                "Overseeing a successful luxury sunglasses store with over 30K frequently updated and SEO optimized products based on Shopify.",
+                "Creating a web scraping tool to collect data from suppliers and sync with a central database utilizing scraping methods with Node.js"
             ],
-            "keywords": [
-                "Team Lead",
-                "Project Manager",
-                "Scrum Master"
-            ]
+            "keywords": []
         },
         {
-            "company": "Pokemon Inc.",
-            "title": "Software Engineer",
-            "years": "2012-2014",
-            "location": "San Francisco, CA",
-            "website": "http://www.mariobros.com",
-            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed odio dui. Nulla vitae elit libero, a pharetra augue.",
+            "company": "Freelance Web Development",
+            "title": "Freelance Web Developer",
+            "years": "September 2011 - December 2018",
+            "location": "Barcelona, Catalonia, Spain",
             "descriptionPoints": [
-                "Worked with a team of 5 to create a new website.",
-                "Planned and executed a new project that increased revenue by 10%."
+                "Built, developed, and maintained a high-traffic website, with over 10M viewers and up to 200k simultaneous viewers.",
+                "Concurrently established several successful e-commerce businesses for Spanish and French companies across various sectors.",
+                "Ensured efficient operations through quick issue resolution and informed decisions on technology, design, logistics, SEO, and SEM."
             ],
-            "keywords": [
-                "Team Lead",
-                "Project Manager",
-                "Scrum Master"
-            ]
-        }
-
+            "keywords": []
+        },
     ],
     "skills": [
         {
-            "name": "HTML",
-            "level": "Expert"
+            "name": "React Native / React",
         },
         {
-            "name": "CSS",
-            "level": "Expert"
+            "name": "GraphQL / REST API",
         },
         {
-            "name": "JavaScript",
-            "level": "Expert"
+            "name": "Node.js / Firebase",
         },
         {
-            "name": "React",
-            "level": "Expert"
-        }
+            "name": "TypeScript / styled-components",
+        },
+      {
+            "name": "E2E, Unit, and Integration Testing",
+      }  
     ],
     "interests": [
         {
-            "name": "Basketball"
+            "name": "Mountain hiking (3.143m record)",
         },
         {
-            "name": "Video Games"
+            "name": "3D Modeling",
         },
         {
-            "name": "Traveling"
+            "name": "Board Games"
+        }, {
+            "name": "Indie Rock Music"
         }
-    ],
-    "references": [
-        {
-            "name": "Jane Doe",
-            "company": "ABC Inc.",
-            "title": "CEO",
-            "phone": "555-555-5555",
-
-        },
-    ],
+        ],
+    "references": [],
     "languages": [
         {
             "name": "English",
-            "level": "Native"
+            "level": "Professional Working"
         },
         {
             "name": "Spanish",
-            "level": "Conversational"
+            "level": "Native"
         },
         {
-            "name": "French",
-            "level": "Conversational"
+            "name": "Catalan",
+            "level": "Native"
         }
     ],
     "i18n": {
@@ -180,6 +146,6 @@ const resume = {
         "interests": "Interests",
         "references": "References",
         "languages": "Languages",
-        "summary": "Summary",
+        "summary": "Summary"
     }
 }
